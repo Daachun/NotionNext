f65533c53f6f1d9be058bbe0592131763f62bc5c
guoxiaochun121892 <guoxiaochun121892@pwrd.com>
2023-03-20 15:36:32 +0800

    本地设置

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -1,15 +1,15 @@
 const CONFIG_NEXT = {
 
   HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
-  HOME_BANNER_Strings: [''], // 首页大图标语文字
+  HOME_BANNER_Strings: ['我们所度过的每个平凡的日常，也许就是连续发生的奇迹'], // 首页大图标语文字
 
-  NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
+  NAV_TYPE: 'autoCollapse', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
-  POST_LIST_COVER: false, // 文章列表显示封面图
+  POST_LIST_COVER: true, // 文章列表显示封面图
   POST_LIST_PREVIEW: true, // 显示文章预览
-  POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
+  POST_LIST_SUMMARY: true, // 显示用户自定义摘要，有预览时优先只展示预览
 
-  POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
+  POST_HEADER_IMAGE_VISIBLE: true, // 文章详情页是否显示封面图
 
   // 右侧组件
   RIGHT_BAR: true, // 是否显示右侧栏
@@ -27,7 +27,7 @@ const CONFIG_NEXT = {
 
   WIDGET_TO_TOP: true, // 是否显示回顶
   WIDGET_TO_BOTTOM: false, // 显示回底
-  WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
+  WIDGET_DARK_MODE: true, // 显示日间/夜间模式切换
   WIDGET_TOC: true, // 移动端显示悬浮目录
 
   ARTICLE_SHARE: process.env.NEXT_PUBLIC_ARTICLE_SHARE || true, // 文章分享功能
